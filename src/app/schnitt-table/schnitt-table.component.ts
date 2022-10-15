ea3bfaea2d8b90962534cb47ad6c294f2c18933f
Niklas Deeg <the_niklas@hotmail.de>
2022-10-15 02:34:05 +0200

    Schnitt-Table Änderungen

--- src/app/schnitt-table/schnitt-table.component.ts
+++ src/app/schnitt-table/schnitt-table.component.ts
@@ -26,7 +26,7 @@ export interface Spieler {
   //, AfterViewInit
 export class SchnittTableComponent implements OnInit {
 
-  displayedColumns = ['name', 'club', 'hspiele', 'hschnitt', 'aspiele', 'aschnitt', 'gspiele', 'gschnitt', 'bestleistung'];
+  displayedColumns = ['name', 'club', 'hschnitt', 'hspiele', 'aschnitt', 'aspiele', 'gschnitt', 'gspiele', 'bestleistung']; // 'gesammtMp', 'heimMp', 'auswaertsMp',
   dataSource!: MatTableDataSource<Spieler>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
